71e7d6c842cb7f231440642a4b8e18c5bbfc7e29
AHHHZ975 <ahz.975@yahoo.com>
2022-03-24 11:19:36 -0400

    Added imageToPatches function.

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -46,3 +46,20 @@ def save_loss_plot(H):
     plt.legend(loc="upper right")
     plt.savefig('TrainResult.jpg')
     # plt.show()  
+
+
+def imageToPatches(inmage, patchSize, isFlattenChannels=True):
+    """
+    Inputs:
+        x - torch.Tensor representing the image of shape [B, C, H, W]
+        patch_size - Number of pixels per dimension of the patches (integer)
+        flatten_channels - If True, the patches will be returned in a flattened format
+                           as a feature vector instead of a image grid.
+    """
+    B, C, H, W = x.shape
+    x = x.reshape(B, C, H//patchSize, patchSize, W//patchSize, patchSize)
+    x = x.permute(0, 2, 4, 1, 3, 5) # [B, H', W', C, p_H, p_W]
+    x = x.flatten(1, 2)              # [B, H'*W', C, p_H, p_W]
+    if isFlattenChannels:
+        x = x.flatten(2,4)          # [B, H'*W', C*p_H*p_W]
+    return x
