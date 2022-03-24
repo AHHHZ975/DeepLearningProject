046581dc2374c3d5988a9e79fc011cfe4ba9cf97
AHHHZ975 <ahz.975@yahoo.com>
2022-03-24 16:02:47 -0400

    Added VisionTransformer class in the NN.py file.

--- NN.py
+++ NN.py
@@ -16,6 +16,8 @@ import torch
 import torch.nn.functional as F
 from torch.nn.modules.conv import ConvTranspose2d
 from torch.nn.modules.flatten import Flatten
+
+from Utils.Utils import imageToPatches
 sys.path.append('C:/Users/AmirHossein/OneDrive/Desktop/DeepLearningProject/DeepLearningProject')
 import Config as cfg
 
@@ -833,5 +835,62 @@ class PreLayerNormAttention(Module):
         x = x + self.attn(inp_x, inp_x, inp_x)[0]
         x = x + self.linear(self.layer_norm_2(x))
         return x
+
+
+class VisionTransformer(Module):
+
+    def __init__(self, embed_dim, hidden_dim, num_channels, num_heads, num_layers, num_classes, patch_size, num_patches, dropout=0.0):
+        """
+        Inputs:
+            embed_dim - Dimensionality of the input feature vectors to the Transformer
+            hidden_dim - Dimensionality of the hidden layer in the feed-forward networks
+                         within the Transformer
+            num_channels - Number of channels of the input (3 for RGB)
+            num_heads - Number of heads to use in the Multi-Head Attention block
+            num_layers - Number of layers to use in the Transformer
+            num_classes - Number of classes to predict
+            patch_size - Number of pixels that the patches have per dimension
+            num_patches - Maximum number of patches an image can have
+            dropout - Amount of dropout to apply in the feed-forward network and
+                      on the input encoding
+        """
+        super().__init__()
+
+        self.patch_size = patch_size
+
+        # Layers/Networks
+        self.input_layer = Linear(num_channels*(patch_size**2), embed_dim)
+        self.transformer = Sequential(*[PreLayerNormAttention(embed_dim, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers)])
+        self.mlp_head = Sequential(
+            LayerNorm(embed_dim),
+            Linear(embed_dim, num_classes)
+        )
+        self.dropout = Dropout(dropout)
+
+        # Parameters/Embeddings
+        self.cls_token = torch.nn.Parameter(torch.randn(1,1,embed_dim))
+        self.pos_embedding = torch.nn.Parameter(torch.randn(1,1+num_patches,embed_dim))
+
+
+    def forward(self, x):
+        # Preprocess input
+        x = imageToPatches(x, self.patch_size)
+        B, T, _ = x.shape
+        x = self.input_layer(x)
+
+        # Add CLS token and positional encoding
+        cls_token = self.cls_token.repeat(B, 1, 1)
+        x = torch.cat([cls_token, x], dim=1)
+        x = x + self.pos_embedding[:,:T+1]
+
+        # Apply Transforrmer
+        x = self.dropout(x)
+        x = x.transpose(0, 1)
+        x = self.transformer(x)
+
+        # Perform classification prediction
+        cls = x[0]
+        out = self.mlp_head(cls)
+        return out
 
 
